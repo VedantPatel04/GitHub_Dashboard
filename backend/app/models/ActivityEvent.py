c60ed93fa72764bde9ec99d944ad992cd11e47b1
Vedant Patel <vedanthp1@gmail.com>
2026-09-02 23:57:42 -0700

    Helper function for SyncRun, test for helper function.

--- backend/app/models/ActivityEvent.py
+++ backend/app/models/ActivityEvent.py
@@ -7,7 +7,7 @@ from sqlalchemy import ForeignKey
 class ActivityEvent(Base):
     __tablename__ = "activity_events"
     id: Mapped[int] = mapped_column(primary_key=True)
-    external_key: Mapped[str] = mapped_column(unique=True)
+    external_key: Mapped[str] = mapped_column(unique=True) #contains the sha commit:{github_repo_id}:{sha}
     event_type: Mapped[str] = mapped_column()
     html_url: Mapped[str | None] = mapped_column()
     occurred_at: Mapped[datetime] = mapped_column()
